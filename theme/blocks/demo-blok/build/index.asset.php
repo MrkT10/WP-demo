a924d9019e31e188a9826cc645dcf39893bd8ca2
Mateusz Markowski <markowski.dev@gmail.com>
2025-10-17 01:20:56 +0200

    demo-block build version update

--- theme/blocks/demo-blok/build/index.asset.php
+++ theme/blocks/demo-blok/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'f800d02f88eecfc9cb10');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '7cdd501a859e056b87bd');
