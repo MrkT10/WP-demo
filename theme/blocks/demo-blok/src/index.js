ae7aabe0a7b52bfb712e718f5db32b2acecf697c
Mateusz Markowski <markowski.dev@gmail.com>
2025-10-20 11:12:01 +0200

    remove atribute overwriting and add empty field check to avoid unexpected content block error

--- theme/blocks/demo-blok/src/index.js
+++ theme/blocks/demo-blok/src/index.js
@@ -1,46 +1,22 @@
 import { registerBlockType } from '@wordpress/blocks';
-import { useState, useEffect } from '@wordpress/element';
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 
 registerBlockType('wpbasestarter/demo-blok', {
-    title: 'Demo Blok',
-    icon: 'edit',
-    category: 'text',
-
-    attributes: {
-        content: { type: 'string' },
-    },
-
     edit({ attributes, setAttributes }) {
         const blockProps = useBlockProps();
-        useEffect(() => {
-            fetch(`${window.location.origin}/wp-json/wpbasestarter/v1/demo-block/1`)
-                .then(res => res.json())
-                .then(data => {
-                    setAttributes({ content: data.content });
-                });
-        }, []);
-
         return (
             <RichText
                 {...blockProps}
                 tagName="p"
                 value={attributes.content}
-                onChange={(newContent) => setAttributes({ content: newContent })}
+                onChange={(value) => setAttributes({ content: value })}
                 placeholder="Wpisz swój tekst do zapisania..."
             />
         );
     },
-
     save({ attributes }) {
         const blockProps = useBlockProps.save();
-
-        return (
-            <RichText.Content
-                {...blockProps}
-                tagName="p"
-                value={attributes.content}
-            />
-        );
-    },
+        if (!attributes.content) return null;
+        return <RichText.Content {...blockProps} tagName="p" value={attributes.content} />;
+    }
 });
