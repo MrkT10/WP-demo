87aa4c3f61795ea5dd33d0b09b74fd5e3ce62de0
Mateusz Markowski <markowski.dev@gmail.com>
2025-10-21 16:03:14 +0200

    change default single post page and add disclaimer

--- theme/single.php
+++ theme/single.php
@@ -9,7 +9,17 @@
 
 get_header();
 ?>
-
+	<section id="disclaimer">
+		<div class="disclaimer-wrapper">
+			<div class="flex flex-col gap-2">
+				<p>Żeby zobaczyć nowy blok przejdź do edycji posta - w blokach gutenberg znajdziesz nowy blok o nazwie <span class="text-highlight">'demo-block'.</span></p>
+				<p>Jeśli na liście nie widzisz bloku o tej nazwie prawdopodobnie zapomniałeś o stworzeniu builda:</span></p>
+				<div class="code w-full">
+					<p>npm run build-block</p>
+				</div>
+			</div>
+		</div>
+	</section>
 	<section id="primary">
 		<main id="main">
 
@@ -19,32 +29,20 @@ get_header();
 				the_post();
 				get_template_part( 'template-parts/content/content', 'single' );
 
-				if ( is_singular( 'post' ) ) {
-					// Previous/next post navigation.
-					the_post_navigation(
-						array(
-							'next_text' => '<span aria-hidden="true">' . __( 'Next Post', 'wpbasestarter' ) . '</span> ' .
-								'<span class="sr-only">' . __( 'Next post:', 'wpbasestarter' ) . '</span> <br/>' .
-								'<span>%title</span>',
-							'prev_text' => '<span aria-hidden="true">' . __( 'Previous Post', 'wpbasestarter' ) . '</span> ' .
-								'<span class="sr-only">' . __( 'Previous post:', 'wpbasestarter' ) . '</span> <br/>' .
-								'<span>%title</span>',
-						)
-					);
-				}
-
-				// If comments are open, or we have at least one comment, load
-				// the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
 				// End the loop.
 			endwhile;
 			?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
+	<section id="back">
+		<div class="disclaimer-wrapper">
+			<div class="flex flex-col gap-2 mb-12">
+				<p>Wróć na stronę główną zeby zobaczyć pozostałe funkcjonalności</p>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">> Strona główna</a>
+			</div>
+		</div>
+	</section>
 
 <?php
 get_footer();
